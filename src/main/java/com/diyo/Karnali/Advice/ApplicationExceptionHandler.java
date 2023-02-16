1b3250b426834b5c8d56f138c6e19b53ad90a7c3
ashimkarki <ashimkarkee91@gmail.com>
2023-02-16 10:18:44 -0500

    Added other methods.

---
+++ src/main/java/com/diyo/Karnali/Advice/ApplicationExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.diyo.Karnali.Advice;
+
+//import com.diyo.Karnali.exception.UserNotFoundException;
+import com.diyo.Karnali.Exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    Map<String, String> handleIllegalArgumentException(MethodArgumentNotValidException ex){
+
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error->{
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserNotFoundException.class)
+    Map<String, String> handleUserNotFoundException(UserNotFoundException ex){
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("error", ex.getMessage());
+
+        return errorMap;
+    }
+
+}
---
+++ src/main/java/com/diyo/Karnali/Exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.diyo.Karnali.Exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
